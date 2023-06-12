d48150a07634ab6d2b3d8ed47ad9c580b5048a4e
Rachana1104 <y.rachana1104@gmail.com>
2023-06-12 21:23:26 +0530

    login and signup

--- student-detail-app/src/auth/TeacherLogin.js
+++ student-detail-app/src/auth/TeacherLogin.js
@@ -1,6 +1,146 @@
-import React from "react";
-export default function TeacherLogin(){
-    return(<>
-    <div><p>Teacher login - email and password</p></div>
-    </>)
+import React, { useState } from "react";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import Paper from "@mui/material/Paper";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
+import axios from "axios"; 
+import {url} from "../utils/api";
+
+const theme = createTheme();
+
+export default function TeacherLogin() {
+  const [setLoading] = useState(true);
+
+  const [values, setValues] = useState({
+    password: "",
+    email: "",
+  });
+
+  const handleChanges = (event) => {
+    setValues({
+      ...values,
+      [event.target.name]: event.target.value,
+    });
+    console.log(values);
+  };
+
+  const handleChange = (prop) => (event) => {
+    setValues({ ...values, [prop]: event.target.value });
+  };
+
+  const history = useNavigate();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get("email"),
+      password: data.get("password"),
+    });
+    createacc();
+  };
+
+  async function createacc() {
+    try {
+      let result = await axios.post(
+        url+"login",
+        {
+          password: values.password,
+          email: values.email,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+          },
+        }
+      );
+      console.log(result.data);
+      if (result.data.allow === true) {
+        Swal.fire("Logged in Successfully!", "success", "success");
+        sessionStorage.setItem("token", result.data.token);
+        history("/");
+      } else {
+        Swal.fire("Oops!!", "Some error while login", "error");
+      }
+    } catch (error) {
+      console.log("Error" + error);
+      setLoading(false);
+    }
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Grid
+        container
+        component="main"
+        sx={{ height: "70vh", width: "100vh", padding: "3rem" }}
+      >
+        <CssBaseline />
+        <Grid item xs={12} sm={8} md={12} component={Paper} elevation={6} square>
+          <Box
+            sx={{
+              my: 8,
+              mx: 4,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <Typography component="h1" variant="h5">
+              Login
+            </Typography>
+            <Box
+              component="form"
+              noValidate
+              onSubmit={handleSubmit}
+              sx={{ mt: 1 }}
+            >
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email"
+                name="email"
+                value={values.email}
+                onChange={handleChanges}
+                autoComplete="name"
+                autoFocus
+                color="secondary"
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                value={values.password.trim()}
+                onChange={handleChange("password")}
+                autoComplete="current-password"
+                color="secondary"
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+                color="secondary"
+              >
+                Login
+              </Button>
+            </Box>
+          </Box>
+        </Grid>
+      </Grid>
+    </ThemeProvider>
+  );
 }
