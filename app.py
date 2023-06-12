49cdf2f66400b0cf99973d4fd308482bff7c00be
PrachetShah <prachetshah25@gmail.com>
2023-06-12 11:22:07 +0530

    Completed Admin Routes and DB Architecture

--- app.py
+++ app.py
@@ -2,23 +2,30 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.exc import IntegrityError
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
-
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///library.db'  # SQLite database file
 db = SQLAlchemy(app)
 
-class User(db.Model):
+class Admin(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100))
     email = db.Column(db.String(100))
-    number = db.Column(db.String(20))
-    userType = db.Column(db.String(50))
+    userType = db.Column(db.String(50), default="admin")
+    password = db.Column(db.String(100))
+
+class Student(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), default="ABC")
+    email = db.Column(db.String(100))
+    number = db.Column(db.String(20), default="0")
+    userType = db.Column(db.String(50), default="student")
 
 with app.app_context():
     db.create_all()
-    # num_rows_deleted = db.session.query(User).delete()
+    # num_rows_deleted = db.session.query(Admin).delete()
+    # num_rows_deleted_two = db.session.query(Student).delete()
     # db.session.commit()
     # print(num_rows_deleted, " Deleted")
 
@@ -26,16 +33,26 @@ with app.app_context():
 def hello_world():
     return 'Hello World'
 
-@app.route('/users', methods=['POST'])
+# ADMIN
+@app.route('/register', methods=['POST'])
 def create_user():
     data = request.get_json()
-    name, email, number, userType = data['name'], data['email'], data['number'], data['userType']
-
-    exists = db.session.query(db.exists().where(User.email == data['email'])).scalar()
+    # name, email, number, userType = data['name'], data['email'], data['number'], data['userType']
+    email = data['email']
+    exists = db.session.query(db.exists().where(Admin.email == data['email'])).scalar()
     print(email, " : ", exists)
 
+    # TODO: Check whether email contains in College Teachers/Admin Library
+
+    hash_and_salted_password = generate_password_hash(
+            data['password'],
+            method='pbkdf2:sha256',
+            salt_length=8
+        )
+
     if(not exists):
-        new_user = User(name=name, email=email, number=number, userType=userType)
+        # new_user = User(name=name, email=email, number=number, userType=userType, password=hash_and_salted_password)
+        new_user = Admin(email=data['email'], password=hash_and_salted_password)
         db.session.add(new_user)
         db.session.commit()
 
@@ -43,9 +60,79 @@ def create_user():
     else:
         return {'message':'Email already exists'}
 
-@app.route('/getUsers')
+@app.route('/login', methods=['POST'])
+def login_user():
+    data = request.get_json()
+    # name, email, number, userType = data['name'], data['email'], data['number'], data['userType']
+    email = data['email']
+    exists = db.session.query(db.exists().where(Admin.email == data['email'])).scalar()
+    print(email, " : ", exists)
+
+    user = Admin.query.filter_by(email=email).first()
+    # print(user)
+
+    if not user:
+        return jsonify({'message': 'User does not exist, Register now'}), 404
+
+    if check_password_hash(user.password, data['password']):
+        allow = check_password_hash(user.password, data['password'])
+        response = {'message': 'Allow Access', 'email': email, 'allow': allow}
+        return jsonify(response), 200
+    else:
+        allow = check_password_hash(user.password, data['password'])
+        response = {'message': 'Wrong Credentials, check Password/Email', 'email': email, 'allow': allow}
+        return jsonify(response), 401
+
+    
+
+@app.route('/getAdmins')
 def get_users():
-    users = User.query.all()
+    users = Admin.query.all()
+    user_list = []
+    for user in users:
+        user_data = {
+            'id': user.id,
+            'email': user.email,
+            'userType': user.userType,
+            'password': user.password
+        }
+        user_list.append(user_data)
+
+    return jsonify(user_list)
+
+'''
+@app.route('/registerStudents', methods=['POST'])
+def create_student():
+    data = request.get_json()
+    # name, email, number, userType = data['name'], data['email'], data['number'], data['userType']
+    email = data['email']
+    exists = db.session.query(db.exists().where(User.email == data['email'])).scalar()
+    print(email, " : ", exists)
+
+    # TODO: Check whether email contains in College Teachers/Admin Library
+
+    hash_and_salted_password = generate_password_hash(
+            data['password'],
+            method='pbkdf2:sha256',
+            salt_length=8
+        )
+
+    if(not exists):
+        # new_user = User(name=name, email=email, number=number, userType=userType, password=hash_and_salted_password)
+        new_user = User(email=data['email'], password=hash_and_salted_password)
+        db.session.add(new_user)
+        db.session.commit()
+
+        return jsonify({'message': 'User created successfully'})
+    else:
+        return {'message':'Email already exists'}
+'''
+
+
+'''
+@app.route('/getStudents')
+def get_users():
+    users = Student.query.all()
     user_list = []
     for user in users:
         user_data = {
@@ -53,16 +140,17 @@ def get_users():
             'name': user.name,
             'email': user.email,
             'number': user.number,
-            'userType': user.userType
+            'userType': user.userType,
+            'password': user.password
         }
         user_list.append(user_data)
 
     return jsonify(user_list)
-
+'''
 
 @app.route('/users/<int:user_id>', methods=['PUT'])
 def edit_user(user_id):
-    user = User.query.get(user_id)
+    user = Student.query.get(user_id)
 
     if not user:
         return jsonify({'message': 'User not found'})
@@ -76,8 +164,6 @@ def edit_user(user_id):
         user.email = data['email']
     if 'number' in data:
         user.number = data['number']
-    if 'userType' in data:
-        user.userType = data['userType']
 
     db.session.commit()
 
