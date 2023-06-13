3cdb15fd4df2505b66399eb744606752a955bcd5
Rachana1104 <y.rachana1104@gmail.com>
2023-06-13 12:26:04 +0530

    login signup ui changed

--- student-detail-app/src/auth/TeacherLogin.js
+++ student-detail-app/src/auth/TeacherLogin.js
@@ -3,14 +3,17 @@ import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
 import TextField from "@mui/material/TextField";
 import Paper from "@mui/material/Paper";
+import Avatar from '@mui/material/Avatar';
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { useNavigate } from "react-router-dom";
 import Swal from "sweetalert2";
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import axios from "axios"; 
 import {url} from "../utils/api";
+import { Link } from "react-router-dom";
 
 const theme = createTheme();
 
@@ -77,22 +80,38 @@ export default function TeacherLogin() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Grid
-        container
-        component="main"
-        sx={{ height: "70vh", width: "100vh", padding: "3rem" }}
-      >
+      <Grid container component="main" sx={{ height: '100vh' }}>
         <CssBaseline />
-        <Grid item xs={12} sm={8} md={12} component={Paper} elevation={6} square>
+        <Grid
+          item
+          xs={false}
+          sm={4}
+          md={7}
+          sx={{
+            backgroundImage:
+              "url(https://img.freepik.com/premium-vector/id-smartphone-learning-metaverse-technology-3d-space-universes-simulation-interface_251139-314.jpg?w=1060)",
+            backgroundRepeat: "no-repeat",
+            backgroundColor: (t) =>
+              t.palette.mode === "light"
+                ? t.palette.grey[50]
+                : t.palette.grey[900],
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        />
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
           <Box
             sx={{
-              my: 8,
+              my: 15,
               mx: 4,
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
             }}
           >
+            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+              <LockOutlinedIcon />
+            </Avatar>
             <Typography component="h1" variant="h5">
               Login
             </Typography>
@@ -100,7 +119,7 @@ export default function TeacherLogin() {
               component="form"
               noValidate
               onSubmit={handleSubmit}
-              sx={{ mt: 1 }}
+              sx={{ mt: 5 }}
             >
               <TextField
                 margin="normal"
@@ -111,7 +130,7 @@ export default function TeacherLogin() {
                 name="email"
                 value={values.email}
                 onChange={handleChanges}
-                autoComplete="name"
+                autoComplete="email"
                 autoFocus
                 color="secondary"
               />
@@ -127,16 +146,18 @@ export default function TeacherLogin() {
                 onChange={handleChange("password")}
                 autoComplete="current-password"
                 color="secondary"
+                sx={{ mt: 3 }}
               />
               <Button
                 type="submit"
                 fullWidth
                 variant="contained"
-                sx={{ mt: 3, mb: 2 }}
+                sx={{ mt: 5, mb: 3 }}
                 color="secondary"
               >
                 Login
               </Button>
+              <Link to="/register">Don't have an account? Sign Up</Link>
             </Box>
           </Box>
         </Grid>
--- student-detail-app/src/auth/TeacherRegister.js
+++ student-detail-app/src/auth/TeacherRegister.js
@@ -3,18 +3,21 @@ import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
 import TextField from "@mui/material/TextField";
 import Paper from "@mui/material/Paper";
+import Avatar from '@mui/material/Avatar';
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { useNavigate } from "react-router-dom";
 import Swal from "sweetalert2";
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import axios from "axios"; 
 import {url} from "../utils/api";
+import { Link } from "react-router-dom";
 
 const theme = createTheme();
 
-export default function TeacherRegister() {
+export default function TeacherLogin() {
   const [setLoading] = useState(true);
 
   const [values, setValues] = useState({
@@ -62,12 +65,12 @@ export default function TeacherRegister() {
         }
       );
       console.log(result.data);
-      if (result.data.message === "User created successfully") {
-        Swal.fire("Registered Successfully!", "success", "success");
+      if (result.data.allow === true) {
+        Swal.fire("Logged in Successfully!", "success", "success");
         sessionStorage.setItem("token", result.data.token);
         history("/");
       } else {
-        Swal.fire("Oops!!", "Some error occurred during registration", "error");
+        Swal.fire("Oops!!", "Some error while login", "error");
       }
     } catch (error) {
       console.log("Error" + error);
@@ -77,22 +80,38 @@ export default function TeacherRegister() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Grid
-        container
-        component="main"
-        sx={{ height: "70vh", width: "100vh", padding: "3rem" }}
-      >
+      <Grid container component="main" sx={{ height: '100vh' }}>
         <CssBaseline />
-        <Grid item xs={12} sm={8} md={12} component={Paper} elevation={6} square>
+        <Grid
+          item
+          xs={false}
+          sm={4}
+          md={7}
+          sx={{
+            backgroundImage:
+              "url(https://img.freepik.com/premium-vector/id-smartphone-learning-metaverse-technology-3d-space-universes-simulation-interface_251139-314.jpg?w=1060)",
+            backgroundRepeat: "no-repeat",
+            backgroundColor: (t) =>
+              t.palette.mode === "light"
+                ? t.palette.grey[50]
+                : t.palette.grey[900],
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        />
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
           <Box
             sx={{
-              my: 8,
+              my: 15,
               mx: 4,
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
             }}
           >
+            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+              <LockOutlinedIcon />
+            </Avatar>
             <Typography component="h1" variant="h5">
               Sign Up
             </Typography>
@@ -100,7 +119,7 @@ export default function TeacherRegister() {
               component="form"
               noValidate
               onSubmit={handleSubmit}
-              sx={{ mt: 1 }}
+              sx={{ mt: 5 }}
             >
               <TextField
                 margin="normal"
@@ -111,7 +130,7 @@ export default function TeacherRegister() {
                 name="email"
                 value={values.email}
                 onChange={handleChanges}
-                autoComplete="name"
+                autoComplete="email"
                 autoFocus
                 color="secondary"
               />
@@ -127,16 +146,18 @@ export default function TeacherRegister() {
                 onChange={handleChange("password")}
                 autoComplete="current-password"
                 color="secondary"
+                sx={{ mt: 3 }}
               />
               <Button
                 type="submit"
                 fullWidth
                 variant="contained"
-                sx={{ mt: 3, mb: 2 }}
+                sx={{ mt: 5, mb: 3 }}
                 color="secondary"
               >
-                Sign Up
+                Signup
               </Button>
+              <Link to="/login">Already have an account? Login</Link>
             </Box>
           </Box>
         </Grid>
