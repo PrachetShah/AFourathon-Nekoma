c7b5efe4ccb36f2c3fd1560c29578dfcf7dd760c
Rachana1104 <y.rachana1104@gmail.com>
2023-06-18 23:25:13 +0530

    all records page integrated

--- src/pages/teacher/AllRecords.js
+++ src/pages/teacher/AllRecords.js
@@ -1,16 +1,31 @@
 import * as React from "react";
 import { DataGrid } from "@mui/x-data-grid";
-import { styled } from "@mui/material/styles";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell, { tableCellClasses } from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
+import { styled } from '@mui/material/styles';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+
 import Paper from "@mui/material/Paper";
 import Delete from "./components/Delete";
 import Update from "./components/Update";
 import CreateStudent from "./CreateStudent";
+import axios from "axios";
+import {url} from "../../utils/api";
+import DeleteIcon from '@mui/icons-material/Delete';
+
+const columns = [
+  { field: "id", headerName: "Student Id", width: 220 },
+  { field: "name", headerName: "Student Name", width: 260 },
+  { field: "email", headerName: "Email Address", width: 350 },
+  {
+    field: "number",
+    headerName: "Phone Number",
+    width: 210,
+  },
+];
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
   [`&.${tableCellClasses.head}`]: {
@@ -23,107 +38,75 @@ const StyledTableCell = styled(TableCell)(({ theme }) => ({
 }));
 
 const StyledTableRow = styled(TableRow)(({ theme }) => ({
-  "&:nth-of-type(odd)": {
+  '&:nth-of-type(odd)': {
     backgroundColor: theme.palette.action.hover,
   },
   // hide last border
-  "&:last-child td, &:last-child th": {
+  '&:last-child td, &:last-child th': {
     border: 0,
   },
 }));
 
-const columns = [
-  { field: "id", headerName: "Student Id", width: 220 },
-  { field: "studentName", headerName: "Student Name", width: 260 },
-  { field: "emailAddress", headerName: "Email Address", width: 350 },
-  {
-    field: "phone",
-    headerName: "Phone Number",
-    width: 210,
-  },
-  // {
-  //   field: 'fullName',
-  //   headerName: 'Full name',
-  //   description: 'This column has a value getter and is not sortable.',
-  //   sortable: false,
-  //   width: 160,
-  //   valueGetter: (params) =>
-  //     `${params.row.firstName || ''} ${params.row.lastName || ''}`,
-  // },
-];
-
-const rows = [
-  { id: 60004200052, studentName: "Rachana Yeldi", emailAddress: "y.rachana1104@gmail.com", phone: 9969821746 },
-  { id: 2, studentName: "Lannister", emailAddress: "Cersei", phone: 42 },
-  { id: 3, studentName: "Lannister", emailAddress: "Jaime", phone: 45 },
-  { id: 4, studentName: "Stark", emailAddress: "Arya", phone: 16 },
-  { id: 5, studentName: "Targaryen", emailAddress: "Daenerys", phone: null },
-  { id: 6, studentName: "Melisandre", emailAddress: null, phone: 150 },
-  { id: 7, studentName: "Clifford", emailAddress: "Ferrara", phone: 44 },
-  { id: 8, studentName: "Frances", emailAddress: "Rossini", phone: 36 },
-  { id: 9, studentName: "Roxie", emailAddress: "Harvey", phone: 65 },
-];
-
-// function createData(name, calories, fat, carbs, protein) {
-//   return { name, calories, fat, carbs, protein };
-// }
-
-// const rows = [
-//   createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-//   createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-//   createData('Eclair', 262, 16.0, 24, 6.0),
-//   createData('Cupcake', 305, 3.7, 67, 4.3),
-//   createData('Gingerbread', 356, 16.0, 49, 3.9),
-// ];
 
 export default function AllRecords() {
+  const [rows, setRows] = React.useState([]);
+
+  React.useEffect(() => {
+    axios
+      .get(url+"getStudents")
+      .then((response) => {
+        setRows(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching students:", error);
+      });
+  }, []);
+
+  const handleDelete = (id) => {
+    axios
+      .post(`${url}student/delete/${id}`)
+      .then(() => {
+        setRows((prevRows) => prevRows.filter((row) => row.id !== id));
+      })
+      .catch((error) => {
+        console.error('Error deleting student:', error);
+      });
+  };
+
+  function Delete({ id, handleDelete }) {
+    const handleClick = () => {
+      handleDelete(id);
+    };
+  
+    return (
+      <DeleteIcon onClick={handleClick} style={{cursor:"pointer"}}></DeleteIcon>
+    );
+  }
+
   return (
     <div
-      style={{
-        height: 400,
-        width: "80%",
-        marginLeft: "10%",
-        marginTop: "2%",
-      }}
+      style={{ height: 650, width: "80%", marginLeft: "10%", marginTop: "0.5%" }}
     >
       <DataGrid
         rows={rows}
-        columns={columns}
-        initialState={{
-          pagination: {
-            paginationModel: { page: 0, pageSize: 5 },
+        columns={[
+          ...columns,
+          {
+            field: 'delete',
+            headerName: 'Delete',
+            width: 120,
+            renderCell: (params) => (
+              <Delete id={params.row.id} handleDelete={handleDelete} />
+            ),
           },
+        ]}
+        initialState={{
+          pagination: { paginationModel: { page: 0, pageSize: 10 } },
         }}
         pageSizeOptions={[5, 10]}
-        checkboxSelection
-        style={{fontSize:"20px"}}
+        // checkboxSelection
+        style={{ fontSize: "20px" }}
       />
     </div>
-    // <TableContainer component={Paper}>
-    //   <Table sx={{ minWidth: 700 }} aria-label="customized table">
-    //     <TableHead>
-    //       <TableRow>
-    //         <StyledTableCell>Dessert (100g serving)</StyledTableCell>
-    //         <StyledTableCell align="right">Calories</StyledTableCell>
-    //         <StyledTableCell align="right">Fat&nbsp;(g)</StyledTableCell>
-    //         <StyledTableCell align="right">Carbs&nbsp;(g)</StyledTableCell>
-    //         <StyledTableCell align="right">Protein&nbsp;(g)</StyledTableCell>
-    //       </TableRow>
-    //     </TableHead>
-    //     <TableBody>
-    //       {rows.map((row) => (
-    //         <StyledTableRow key={row.name}>
-    //           <StyledTableCell component="th" scope="row">
-    //             {row.name}
-    //           </StyledTableCell>
-    //           <StyledTableCell align="right">{row.calories}</StyledTableCell>
-    //           <StyledTableCell align="right">{row.fat}</StyledTableCell>
-    //           <StyledTableCell align="right">{row.carbs}</StyledTableCell>
-    //           <StyledTableCell align="right">{row.protein}</StyledTableCell>
-    //         </StyledTableRow>
-    //       ))}
-    //     </TableBody>
-    //   </Table>
-    // </TableContainer>
   );
 }
