932c23f50b85c02dec7c47e7782ce415d1f1032c
PrachetShah <prachetshah25@gmail.com>
2023-07-08 13:32:20 +0530

    wrote some more tests

--- test_admin.py
+++ test_admin.py
@@ -1,7 +1,9 @@
 from app import app # Flask instance of the API
 import json
+import random
+import string
 
-def test_register_admin():
+def test_register_admin_known():
     mimetype = 'application/json'
     headers = {
         'Content-Type': mimetype,
@@ -14,6 +16,21 @@ def test_register_admin():
     assert type(response.data.decode('utf-8')) == str 
     assert response.content_type == mimetype
     assert response.status_code == 401
+
+def test_register_admin_unknown():
+    mimetype = 'application/json'
+    headers = {
+        'Content-Type': mimetype,
+    }
+    res = ''.join(random.choices(string.ascii_lowercase, k=7))
+    res2 = ''.join(random.choices(string.ascii_lowercase+string.ascii_uppercase+string.digits, k=9))
+    data = {
+        'email': f'{res}@gmail.com',
+        'password': f'{res2}',
+    }
+    response = app.test_client().post('/register', data=json.dumps(data), headers=headers)
+    assert response.content_type == mimetype
+    assert response.status_code == 200
 
 def test_get_all_admin():
     response = app.test_client().get('/getAdmins')
